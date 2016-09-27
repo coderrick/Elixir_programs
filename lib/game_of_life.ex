436a47fbcec71a21f72fc3fe5cfd942066fe92b8
coderrick <derrick.duncan@temple.edu>
2016-09-26 23:51:55 -0400

    Added game of life file

---
+++ lib/game_of_life.ex
@@ -0,0 +1,29 @@
+defmodule GameOfLife do
+
+  def printboard(b) do
+    Enum.each(b, fn(x) -> 
+      Enum.each(x, fn(y) -> 
+        IO.write y
+      end)
+      IO.puts ""
+    end)
+  end
+
+  def nextboard(b) do
+    Enum.map(b, fn(row) -> 
+      Enum.map(row, fn(cell) -> 
+        cell 
+      end)
+    end)
+  end
+end
+
+startboard=[[0,0,1,0,0], 
+            [1,0,1,0,0], 
+            [1,0,0,0,0], 
+            [1,0,1,1,0], 
+            [1,0,1,0,0]]
+
+GameOfLife.printboard(startboard)
+IO.puts ""
+GameOfLife.printboard(GameOfLife.nextboard(startboard))
